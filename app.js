1408042572adec1e328d30a6db9d74b7ce058162
full-stack-david <davidchan12323@gmail.com>
2023-09-20 03:31:24 -0500

    Fetch API data and generate result

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const https = require('https');
 
 const app = express();
 const limiter = rateLimit({
@@ -9,8 +12,51 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-app.get('/', async(req, res) => {
-  return res.status(200).json({success: 'Success'});
+app.get('/api/fetch', async(req, res) => {
+  try {
+    const functions = [
+      fetch('https://api.dictionaryapi.dev/api/v2/entries/en/apple'),
+      fetch('https://api.dictionaryapi.dev/api/v2/entries/en/peach'),
+      fetch('https://api.dictionaryapi.dev/api/v2/entries/en/pear')
+    ];
+
+    const response = await Promise.all(functions);
+
+    let writeData = '[';
+
+    const data = await Promise.all(response.map(async(item) => {
+      const result = await item.json();
+      const phonetics = result[0].phonetics;
+      for (let i = 0; i < phonetics.length; i++) {
+        const audio = phonetics[i].audio.split('/');
+        const filename = audio[audio.length - 1];
+
+        if (!fs.existsSync('audio/' + filename)) {
+          const file = fs.createWriteStream('audio/' + filename);
+
+          https.get(phonetics[i].audio, function(response) {
+            response.pipe(file);
+
+            file.on("finish", () => {
+              file.close();
+            });
+          });
+        }
+      }
+
+      writeData += JSON.stringify(result[0]) + ',';
+
+      return result;
+    }));
+
+    writeData = writeData.slice(0, -1) + ']';
+
+    fs.appendFileSync('words.json', writeData);
+
+    return res.status(200).json({data});
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
